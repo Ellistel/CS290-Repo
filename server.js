348a12ae8dfe0d3371d2a006b2bd888b4c9e8452
Luke Ellister <ellistel@oregonstate.edu>
2023-12-09 15:59:12 -0800

    started working on handlebars content generation for comments

--- server.js
+++ server.js
@@ -3,15 +3,33 @@ var path = require('path')
 var express = require('express')
 var exphbs = require ("express-handlebars")
 var fs = require('fs');
+var commentData = require("./commentData.json")
 
 
 var app = express()
 var port = process.env.PORT || 3000
 
-app.engine("handlebars", exphbs.engine({defaultLayout: "main"}))
+app.engine("handlebars", exphbs.engine({defaultLayout: false}))
 app.set("view engine", "handlebars")
 
 app.use(express.static('static'))
+
+
+
+
+
+
+app.get("/test", function(req,res,next)
+{
+    res.status(200).render("specificblogpost", {
+        posts: commentData,
+        
+    })
+})
+
+
+
+
 
 console.log("listening on port: 3000")
 
