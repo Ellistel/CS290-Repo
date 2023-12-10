3e9a4408d3481eed768ceb83bfe75c268df6190e
Luke Ellister <ellistel@oregonstate.edu>
2023-12-09 16:26:19 -0800

    added dynamic content generation functions for navigating from the main blog post page to an individual post

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express')
 var exphbs = require ("express-handlebars")
 var fs = require('fs');
 var commentData = require("./commentData.json")
+var blogData = require("./blogpostData.json")
 
 
 var app = express()
@@ -15,17 +16,37 @@ app.set("view engine", "handlebars")
 app.use(express.static('static'))
 
 
-
+app.get("/testmain", function(req,res,next)
+{ 
+    res.status(200).render("mainpage", {
+        posts: blogData,
+        
+    })
+})
 
 
 
 app.get("/test", function(req,res,next)
-{
+{ 
     res.status(200).render("specificblogpost", {
         posts: commentData,
         
     })
 })
+
+app.get("/:postnumber", function(req,res,next)
+{ 
+    var number = req.params.postnumber
+    var postTodisplay = blogData[number - 1]
+    res.status(200).render("specificblogpost", {
+        
+        posts: [postTodisplay],
+        comments: commentData,
+        
+    })
+})
+
+
 
 
 
