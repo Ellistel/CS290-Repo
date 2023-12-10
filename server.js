4673c43563f5d3880df3b1702eabf763dbf7b739
Luke Ellister <ellistel@oregonstate.edu>
2023-12-09 19:17:22 -0800

    Added server data JSON object and hooked it up with server

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var express = require('express')
 var exphbs = require ("express-handlebars")
 var fs = require('fs');
 var commentData = require("./commentData.json")
-var blogData = require("./blogpostData.json")
+var blogData = require("./blogpostData.json");
+const e = require('express');
+serverData = require("./serverData.json")
 
 
 var app = express()
@@ -17,9 +19,11 @@ app.use(express.static('static'))
 
 
 app.get("/testmain", function(req,res,next)
-{ 
+{
+    console.log("serverData", serverData)
     res.status(200).render("mainpage", {
-        posts: blogData,
+        
+        posts: serverData,
         
     })
 })
@@ -36,16 +40,36 @@ app.get("/test", function(req,res,next)
 
 app.get("/:postnumber", function(req,res,next)
 { 
+
     var number = req.params.postnumber
-    var postTodisplay = blogData[number - 1]
+    var keysArray = Object.keys(serverData)
+    console.log("Length:", keysArray.length)
+    console.log("number", number)
+    if(keysArray.length >= number )
+    {
+    var postTodisplay = serverData[number - 1]
+    console.log("post to display:", postTodisplay)
+     if(!(isNaN(number)) && !number.includes('.') &&  (parseInt(number) >= 1 && parseInt(number) <= 8) )
+    {
     res.status(200).render("specificblogpost", {
         
         posts: [postTodisplay],
-        comments: commentData,
-        
+        comments: postTodisplay.comments
+       
     })
+
+ }
+}
+else
+next()
 })
 
+app.get("*", function(req,res)
+{
+
+    res.status(404).send("Page Not Found")
+
+})
 
 
 
